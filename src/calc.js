76522dcc05eed7ab060ef63b04d5ef43321175cc
santosh <santosh.ece06@gmail.com>
2021-07-26 10:32:20 +0530

    adv.js

---
+++ src/calc.js
@@ -0,0 +1,9 @@
+function sum(n1, n2) {
+  return n1 + n2;
+}
+
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+export { sum };
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { sum } from "./calc.js";
+
+let output = sum(10, 30);
+console.log(output);
